d284ab3d2facef4a995cb1615ef1e4d4fbd04ee5
Nathata <nathan.pbombana@gmail.com>
2019-03-07 22:16:19 -0300

    Primitive authentication system

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<?php
-    try {
-        $conexao = new PDO("mysql:host=localhost;dbname=contatos", "root", "");
-        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $conexao->exec("set names utf8");
-    } catch (PDOException $erro) {
-        ?>
-            <script>
-                //window.alert(`Falha ao Conectar\n<?= $erro->getMessage()?>`);
-            </script>
-
-        <?php
-    }
-?>
 <!DOCTYPE html>
 <html lang="pt">
 <head>
@@ -21,6 +7,48 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <div id="db-info-dialog" title="Database Connection" style="display:none;">
+        <h3>Database Credentials:</h3>
+        <form action="" method="post">
+            <fieldset>
+                <label for="dbname">Name:</label><br>
+                <input type="text" name="dbname" id="dbname" required>
+                <br>
+                <label for="dbuser">User:</label><br>
+                <input type="text" name="dbuser" id="dbuser" required>
+                <br>
+                <label for="dbpwd">Password:</label><br>
+                <input type="password" id="dbpwd" name="dbpwd">
+                <br>
+                <input type="submit">
+            </fieldset>
+        </form>
+    </div>
+<?php
+    try {
+        if(empty($_REQUEST['dbname']) || empty($_REQUEST['dbuser'])){
+    ?>
+        <script>
+          document.querySelector('#db-info-dialog').style.display = 'initial';
+          window.stop();
+        </script>
+    <?php
+        }
+
+        $conexao = new PDO("mysql:host=localhost;dbname={$_REQUEST['dbname']}", $_REQUEST['dbuser'], $_REQUEST['dbpwd']);
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conexao->exec("set names utf8");
+    } catch (PDOException $erro) {
+        ?>
+            <script>
+                window.alert(`Falha ao Conectar\n<?= $erro->getMessage()?>`);
+                window.location.href = window.location.href;
+            </script>
+
+        <?php
+    }
+?>
     <div class="container-fluid h-100 d-flex">
         <div class="w-100 h-100 d-flex flex-column">
             <div class="row">
@@ -72,7 +100,6 @@
             </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script>$('[data-toggle="tooltip"]').tooltip()</script>
