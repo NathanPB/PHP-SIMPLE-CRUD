ed6f8a48857369cf008fd2bcbf4f0eafbadecb44
NathanPB <nathan.pbombana@gmail.com>
2019-05-29 12:22:00 -0300

    API endpoint for manipulating contacts

---
+++ api/cliente/contato/adicionar/index.php
@@ -0,0 +1,28 @@
+<?php
+    include_once '../../../../includes/api.php';
+    include_once '../../../../includes/authentication.php';
+
+    try {
+        if(isset($_REQUEST['value'])) {
+            if (isset($_REQUEST['id'])) {
+                $sth = $db->prepare('update contato set value = :value where id = :id');
+                $sth->execute(array(
+                    'value' => $_REQUEST['value'],
+                    'id' => $_REQUEST['id']
+                ));
+            } else if (isset($_REQUEST['cliente'])) {
+                $sth = $db->prepare('insert into contato (value, cliente, type) values (:value, :cliente, :type)');
+                $sth->execute(array(
+                    'value' => $_REQUEST['value'],
+                    'cliente' => $_REQUEST['cliente'],
+                    'type' => strpos($_REQUEST['value'], '@') != false ? 'E' : 'T'
+                ));
+            } else {
+                throw new Exception('missing parameters');
+            }
+        } else {
+            throw new Exception('missing parameters');
+        }
+    } catch (Exception $ex) {
+        echo '{"message":"'.$ex->getMessage().'"}';
+    }
